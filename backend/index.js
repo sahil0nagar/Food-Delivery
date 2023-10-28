f8858c1f1b5f5051fd8e393e87f80a96b30c0535
Sahil Nagar <nagarsahil2000@gmail.com>
2023-10-28 12:17:10 +0530

    feat:added atlas

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.json());
 
 mongoose
-  .connect("mongodb://localhost:27017/fooddelivery")
+  .connect("mongodb+srv://nagarsahil2000:k772qT4hOtKNCtX8@cluster0.yspqlt5.mongodb.net/?retryWrites=true&w=majority")
   .then(() => {
     console.log("Connected");
   })
